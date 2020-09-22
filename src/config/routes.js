b29d286052fb43cb5217dde2eb256810bdcbff97
Linykeer Almeida <linykeerrenato@gmail.com>
2020-09-21 22:25:14 -0300

    Projeto Finalizado e entregue

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
-module.exports = function(server) {
+module.exports = function (server) {
     const router = express.Router()
     server.use('/api', router)
 
     const todoService = require('../api/todo/todoService')
     todoService.register(router, '/todos')
+    const todoCadastro = require('../api/cadastro/todoCadastro')
+    todoCadastro.register(router, '/cadastros')
 }
